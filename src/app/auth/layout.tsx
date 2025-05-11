b46807016e73da3127c478da3607cc11f79f0bbc
Gamingstein <timeblackhole980@gmail.com>
2025-05-11 14:13:22 +0530

    clearance

--- src/app/auth/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <section className="h-dvh">{children}</section>;
-}
--- src/app/mobile/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function MobileLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <section className="h-dvh">{children}</section>;
-}
